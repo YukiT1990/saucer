a66ce36494acb6f31c716324c401121b52ccd538
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2020-04-23 14:42:46 +0900

    saucer

---
+++ sketch.js
@@ -0,0 +1,94 @@
+//Topic 1.1 
+//Object orientation revisted
+//part one
+
+//var flying_saucer_x;
+//var flying_saucer_y;
+//var flying_saucer_width;
+//var flying_saucer_height;
+
+var flying_saucer;
+
+function setup()
+{
+    createCanvas(800,600);
+    noStroke();
+    
+//    flying_saucer_x = 400;
+//    flying_saucer_y = 150;
+//    flying_saucer_width = 150;
+//    flying_saucer_height = 50;
+    
+    flying_saucer = {
+        x: 400,
+        y: 150,
+        width: 250,
+        height: 75,
+        window_width: 0.5,
+        window_height: 1.2,
+        base_height: 0.45,
+        num_lights: 20,
+        brightnesses: []
+    }
+    
+    for(var i = 0; i < flying_saucer.num_lights; i++)
+    {
+        flying_saucer.brightnesses.push((i * 10)%225);
+    }
+}
+
+function draw()
+{
+    background(50,0,80);
+    
+    //draw the ground
+    fill(0,50,0);
+    rect(0,height - 100, width, 100);
+    
+    //draw the flying saucer
+    fill(175,238,238);
+    arc(
+        flying_saucer.x,
+        flying_saucer.y,
+        flying_saucer.width * flying_saucer.window_width,
+        flying_saucer.height * flying_saucer.window_height,
+        PI,
+        TWO_PI);
+    fill(150);
+    arc(
+        flying_saucer.x,
+        flying_saucer.y,
+        flying_saucer.width,
+        flying_saucer.height/2,
+        PI,
+        TWO_PI);
+    fill(50);
+    arc(
+        flying_saucer.x,
+        flying_saucer.y,
+        flying_saucer.width,
+        flying_saucer.height * flying_saucer.base_height,
+        0,
+        PI);
+    
+    flying_saucer.x += random(-2, 2);
+    flying_saucer.y += random(-1, 1);
+    
+    
+    
+    fill(255);
+    
+    var incr = flying_saucer.width/(flying_saucer.num_lights - 1);
+    
+    for(var i = 0; i < flying_saucer.num_lights; i++)
+    {
+        fill(flying_saucer.brightnesses[i]);
+        ellipse(
+            flying_saucer.x - flying_saucer.width/2 + incr * i, 
+            flying_saucer.y, 
+            5);
+        flying_saucer.brightnesses[i] += 2;
+        flying_saucer.brightnesses[i] = flying_saucer.brightnesses[i]%225;
+    }
+    
+}
